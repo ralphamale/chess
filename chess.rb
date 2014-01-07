46d9308a95eb0f18a7c0e7241f0452a43d382068
Ralph Nguyen and Alex Harris <ralph.nguyen@gmail.com and harris.alexk@gmail.com>
2014-01-07 10:35:49 -0500

    Finishes moves/move_dirs for every piece but pawn

--- chess.rb
+++ chess.rb
@@ -1,3 +1,5 @@
+ROWS = 8
+
 class Piece
   attr_accessor :pos, :board
 
@@ -8,37 +10,91 @@ class Piece
 end
 
 class SlidingPiece < Piece
+
+  # def neighbors
+  #   adjacent_coords = DELTAS.map do |(dx, dy)|
+  #     [pos[0] + dx, pos[1] + dy]
+  #   end.select do |row, col|
+  #     [row, col].all? do |coord|
+  #       coord.between?(0, @board.grid_size - 1)
+  #     end
+  #   end
+  #
+  #   adjacent_coords.map { |pos| @board[pos] }
+  # end
+
   def moves
-    # calls on move_dirs
+    possible_moves = []
+    i = 1
+    while i < ROWS
+      possible_moves += move_dirs.map do |(dx, dy)|
+        [pos[0] + (i*dx), pos[1] + (i*dy)]
+      end
 
+      i += 1
+    end
 
-
-    # return an array of places a Piece can move to
+    possible_moves.select do |row, col|
+      [row, col].all? do |coord|
+        coord.between?(0, ROWS-1)
+      end
+    end
   end
 
+      #adjacent_coords.map { |pos| @board[pos] } <- fr/ minesweep
+      # return an array of places a Piece can move to
 
 end
 
 class Bishop < SlidingPiece
   def move_dirs
-    # diagonal
+    [[1,1], [1, -1], [-1, -1], [-1, 1]]
   end
 end
 
 class Rook < SlidingPiece
   def move_dirs
-    #horizontal,vertical
+    [[1,0], [-1, 0], [0, -1], [0, 1]]
   end
 end
 
 class Queen < SlidingPiece
+
   def move_dirs
-    # horizontal, vertical, diagonal
+    [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1],
+    [1, -1], [1, 0], [1, 1]]
   end
 end
 
 class SteppingPiece < Piece
+  def moves
 
+    possible_moves = []
+      possible_moves += move_dirs.map do |(dx, dy)|
+        [pos[0] + dx, pos[1] + dy]
+      end.select do |row, col|
+      [row, col].all? do |coord|
+        coord.between?(0, ROWS-1)
+      end
+    end
+
+
+  end
 end
 
-#Class Pawn < Piece
+class Knight < SteppingPiece
+  def move_dirs
+    [[1,2], [2,1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+  end
+end
+
+class King < SteppingPiece
+  def move_dirs
+    [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1],
+    [1, -1], [1, 0], [1, 1]]
+  end
+end
+
+
+class Pawn < Piece
+end
